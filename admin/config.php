57f2c028a582aecf4027abcffdf39c3723f0200c
Daniel <daniel.statera@gmail.com>
2017-03-27 17:38:46 +0300

    Рабочая версия OpenCart

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://dneprkrovlya.testfl.dp.ua/admin/');
+define('HTTP_CATALOG', 'http://dneprkrovlya.testfl.dp.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://dneprkrovlya.testfl.dp.ua/admin/');
+define('HTTPS_CATALOG', 'http://dneprkrovlya.testfl.dp.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/home/testfl/testfl.dp.ua/dneprkrovlya/admin/');
+define('DIR_SYSTEM', '/home/testfl/testfl.dp.ua/dneprkrovlya/system/');
+define('DIR_IMAGE', '/home/testfl/testfl.dp.ua/dneprkrovlya/image/');
+define('DIR_LANGUAGE', '/home/testfl/testfl.dp.ua/dneprkrovlya/admin/language/');
+define('DIR_TEMPLATE', '/home/testfl/testfl.dp.ua/dneprkrovlya/admin/view/template/');
+define('DIR_CONFIG', '/home/testfl/testfl.dp.ua/dneprkrovlya/system/config/');
+define('DIR_CACHE', '/home/testfl/testfl.dp.ua/dneprkrovlya/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/testfl/testfl.dp.ua/dneprkrovlya/system/storage/download/');
+define('DIR_LOGS', '/home/testfl/testfl.dp.ua/dneprkrovlya/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/testfl/testfl.dp.ua/dneprkrovlya/system/storage/modification/');
+define('DIR_UPLOAD', '/home/testfl/testfl.dp.ua/dneprkrovlya/system/storage/upload/');
+define('DIR_CATALOG', '/home/testfl/testfl.dp.ua/dneprkrovlya/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'testfl.mysql.ukraine.com.ua');
+define('DB_USERNAME', 'testfl_dpkrovlya');
+define('DB_PASSWORD', 'awp9bfag');
+define('DB_DATABASE', 'testfl_dpkrovlya');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://dneprkrovlya.testfl.dp.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://dneprkrovlya.testfl.dp.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/home/testfl/testfl.dp.ua/dneprkrovlya/catalog/');
+define('DIR_SYSTEM', '/home/testfl/testfl.dp.ua/dneprkrovlya/system/');
+define('DIR_IMAGE', '/home/testfl/testfl.dp.ua/dneprkrovlya/image/');
+define('DIR_LANGUAGE', '/home/testfl/testfl.dp.ua/dneprkrovlya/catalog/language/');
+define('DIR_TEMPLATE', '/home/testfl/testfl.dp.ua/dneprkrovlya/catalog/view/theme/');
+define('DIR_CONFIG', '/home/testfl/testfl.dp.ua/dneprkrovlya/system/config/');
+define('DIR_CACHE', '/home/testfl/testfl.dp.ua/dneprkrovlya/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/testfl/testfl.dp.ua/dneprkrovlya/system/storage/download/');
+define('DIR_LOGS', '/home/testfl/testfl.dp.ua/dneprkrovlya/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/testfl/testfl.dp.ua/dneprkrovlya/system/storage/modification/');
+define('DIR_UPLOAD', '/home/testfl/testfl.dp.ua/dneprkrovlya/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'testfl.mysql.ukraine.com.ua');
+define('DB_USERNAME', 'testfl_dpkrovlya');
+define('DB_PASSWORD', 'awp9bfag');
+define('DB_DATABASE', 'testfl_dpkrovlya');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
